5dc49673408542b8c6b2904901c222a115c8ab5e
Ondrej Stastny <StastnyO@gmail.com>
2011-11-01 00:53:48 +0100

    Pridany akce pro detail Cloveka a Rozvrh

---
+++ server/FellowMe/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+namespace FellowMe
+{
+    public static class Helpers
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+
+        public static int WeekNumber(this DateTime date)
+        {
+            GregorianCalendar cal = new GregorianCalendar(GregorianCalendarTypes.Localized);
+            return cal.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
