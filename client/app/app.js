12bfbfae02fa5a7b550fa5e53e92eb7b6b2087b6
premysl.vyhnal <premysl.vyhnal@localhost>
2011-11-08 00:21:30 +0100

    client -- vibrujici tlacitka

--- client/app/app.js
+++ client/app/app.js
@@ -5,6 +5,16 @@ FellowMe.config = {
 };
 
 
+if(typeof(Device)==='undefined'){
+	Device = {
+		vibrate: Ext.emptyFn,
+		showToast: window.alert,
+		getModel: Ext.emptyFn,
+		getManufacturer: Ext.emptyFn,
+		getUUID: Ext.emptyFn,
+	};
+}
+
 document.title = FellowMe.config.windowTitle;
 // XXX: vyhodit
 var fnLog = function(){console.log(arguments)}
