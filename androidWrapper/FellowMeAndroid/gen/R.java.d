4f29e83f2e82af70c777de57f316d5a7f84f33ab
Ondrej Stastny <StastnyO@gmail.com>
2011-12-05 19:23:15 +0100

    client: ikony server: vyhledavani bez diakritiky, uprava vystupu

---
+++ androidWrapper/FellowMeAndroid/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/premek/NetBeansProjects/FellowMeAndroid/gen/android/fellowme/R.java \
+ : /home/premek/NetBeansProjects/FellowMeAndroid/res/values/strings.xml \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/layout/main.xml \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/menu/mainmenu.xml \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/xml/preferences.xml \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/drawable-mdpi/preferences.png \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/drawable-mdpi/icon.png \
+/home/premek/NetBeansProjects/FellowMeAndroid/res/drawable-mdpi/refresh.png \
+/home/premek/NetBeansProjects/FellowMeAndroid/AndroidManifest.xml \
